43c0d11a52288458136590c500f6a04985c052c1
Paras <92735871+Paras0111@users.noreply.github.com>
2026-05-16 23:22:36 +0530

    Add files via upload

---
+++ Ring.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{   
+
+    /**
+    Implementing a ring buffer 
+    1. Size n 
+    2. Push pop
+    3. Head - points to the element removed
+    4. Tail - points to the next empty cell
+    5. Tail points to start of the cell if reaches end , hence circular buffer.
+    6. head also same, points back to the start.
+    */
+    cout << "hello world" << endl;
+
+    return 0;
+}
